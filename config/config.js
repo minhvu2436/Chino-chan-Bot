bd54a9dd10444896f302fa70d2320ce7187ef2fd
minhvu2436 <86639426+minhvu2436@users.noreply.github.com>
2023-03-29 22:42:02 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const NodeCache = require('node-cache')
 
 const config = {
     prefix: '/',
-    openaikey: '',
+    openaikey: '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',
     cache: new NodeCache({
         checkperiod: 10000,
         deleteOnExpire: true
